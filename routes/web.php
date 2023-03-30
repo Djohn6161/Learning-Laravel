972b62c9e748cf71e211a49a250e6cd7acfcd373
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2023-03-30 08:53:43 +0800

    intial Learnings

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+lopremipsum
+*/
+// all Listings
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Jobs',
+        'listings' => Listing::all()
+    ]);
+});
+// Single Listing
+Route::get('/listing/{id}', function($id){
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
+
+
+
+
+
+// Route::get('/hello', function(){
+//     return response('<h1>Hello World</h1>')
+//     ->header('Content-Type', 'text/plain')
+//     ->header('foo', 'bar');
+// });
+// Route::get('/posts/{id}', function($id){
+//     // ddd($id);
+//     return response('Post ' . $id);
+// })->where('id', '[0-9]+');
+// Route::get('/search', function(Request $request){
+//     // dd($request->name . ' ' . $request->city);
+//     return $request->name . ' ' . $request->city;
+// });
